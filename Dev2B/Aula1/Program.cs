d968f13a0142f1230990cea81fcd884fe32b0e1c
SMARAPD - Gabriel Beserra <GTBESERRA@SMARAPD.COM.BR>
2021-06-22 17:34:17 -0300

    Feito exercicio 1

--- Dev2B/Aula1/Program.cs
+++ Dev2B/Aula1/Program.cs
@@ -6,7 +6,29 @@ namespace Aula1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Exercicio1();
+        }
+
+        static void Exercicio1()
+        {
+            Console.Write("Informe a nota: ");
+
+            string entrada = Console.ReadLine();
+            decimal nota = decimal.Parse(entrada);
+
+            if (nota >= 7 && nota <= 10)
+            {
+                Console.WriteLine("Aprovado!");
+            }
+            else if (nota >= 4 && nota < 7)
+            {
+                Console.WriteLine("Exame!");
+            }
+            else if (nota >= 0 && nota < 4)
+            {
+                Console.WriteLine("Reprovado!");
+            }
+
         }
     }
 }
