5a325951a32721bd782bf980073b633cd2f626bc
Bruno-Camargo <brunobbb_stz@hotmail.com>
2021-06-23 12:01:22 -0300

    Subindo os exercicios

---
+++ Dev2B/Aula1/Pessoa.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Aula1 {
+    class Pessoa {
+        public int idade { get; set; }
+        public decimal peso { get; set; }
+        public decimal altura { get; set; }
+    }
+}
