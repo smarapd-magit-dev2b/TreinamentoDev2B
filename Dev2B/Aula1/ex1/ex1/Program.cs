a8349668f0d922c5b7454e47eb768158562e7a16
SMARAPD - Malthus Cunha <MVCUNHA@SMARAPD.COM.BR>
2021-06-23 14:05:54 -0300

    Exercicio 1

---
+++ Dev2B/Aula1/ex1/ex1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Digite sua media: ");
+			int media = Convert.ToInt32(Console.ReadLine());
+
+			if (media >= 7 && media <= 10)
+			{
+				Console.Write("Aprovado");
+				Console.ReadLine();
+			}
+			else {
+				if (media >= 4 && media <= 6.99)
+				{
+					Console.Write("Exame");
+					Console.ReadLine();
+				}
+				else
+				{
+					Console.Write("Reprovado");
+					Console.ReadLine();
+				}
+			}
+		}
+	}
+}
