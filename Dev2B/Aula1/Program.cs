6f02c568a0df67ab53d8e4ffd24247169f55ea2d
Yuri-Fabio-Sanches <yuriusp2011@hotmail.com>
2021-06-23 11:16:24 -0300

    Resolução exercicio 1

--- Dev2B/Aula1/Program.cs
+++ Dev2B/Aula1/Program.cs
@@ -6,7 +6,29 @@ namespace Aula1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            exercicio1();
+            Console.ReadKey();
+        }
+
+        static void exercicio1()
+        {
+            decimal media;
+
+            Console.Write("Digite a media do aluno: ");
+            media = Convert.ToDecimal(Console.ReadLine());
+
+            if (media >= 7.0m && media <= 10.0m)
+            {
+                Console.WriteLine("Aprovado!");
+            }
+            else if (media >= 4.0m && media <= 6.99m)
+            {
+                Console.WriteLine("Exame!");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado!");
+            }
         }
     }
 }
