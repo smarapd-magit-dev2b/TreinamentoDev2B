c05e0237a647719463be6e32a8c09ad35d7683ef
Bruno-Camargo <brunobbb_stz@hotmail.com>
2021-06-22 21:51:20 -0300

    Exercicio01

--- Dev2B/Aula1/Program.cs
+++ Dev2B/Aula1/Program.cs
@@ -6,7 +6,7 @@ namespace Aula1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!");
         }
     }
 }
