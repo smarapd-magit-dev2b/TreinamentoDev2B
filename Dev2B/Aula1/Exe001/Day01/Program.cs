022bccd1e7cf5671bd6e4d6581ca89eebb13d2d9
Bruno-Camargo <brunobbb_stz@hotmail.com>
2021-06-22 21:57:24 -0300

    Exercicio1 adicionado

---
+++ Dev2B/Aula1/Exe001/Day01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool rodando = false;
+            decimal nota = 0;
+
+            while (rodando)
+            {
+                Console.Write("Digite a nota do aluno: ");
+                nota = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (nota > 10)
+                {
+                    Console.WriteLine("Nota invalida !!");
+                }
+                else
+                {
+                    rodando = true;
+                }
+            }
+
+            if(nota >= 7)
+            {
+                Console.WriteLine("Aprovado !!");
+            }
+            else if(nota >= 4 && nota <= 6.99m)
+            {
+                Console.WriteLine("Exame !!");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado !!");
+            }
+
+
+
+        }
+    }
+}
