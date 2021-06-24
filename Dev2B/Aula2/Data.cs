469d08ae857b09ca92c8ac5a402a5e1f83c4556c
Yuri-Fabio-Sanches <yuriusp2011@hotmail.com>
2021-06-24 13:35:08 -0300

    Aula 2 - Exercícios 1,2,3

---
+++ Dev2B/Aula2/Data.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula2
+{
+    public class Data
+    {
+        public int Dia { get; set; }
+        public int Mes { get; set; }
+        public int Ano { get; set; }
+
+        public Data()
+        {
+
+        }
+
+        public Data(int dia, int mes, int ano)
+        {
+            bool validador = false;
+
+            do
+            {
+                if (dia >= 1 && dia <= 30)
+                {
+                    validador = true;
+                }
+
+                else
+                {
+                    Console.WriteLine("Dia inválido. Insira um novo dia");
+                    dia = Convert.ToInt32(Console.ReadLine());
+                }
+            } while (!validador);
+            Dia = dia;
+
+            validador = false;
+            do
+            {
+                if (mes >= 1 && mes <= 12)
+                {
+                    validador = true;
+                }
+
+                else
+                {
+                    Console.WriteLine("Mês inválido. Insira um novo mês");
+                    mes = Convert.ToInt32(Console.ReadLine());
+                }
+            } while (!validador);
+            Mes = mes;
+
+            do
+            {
+                if (ano > 0)
+                {
+                    validador = true;
+                }
+
+                else
+                {
+                    Console.WriteLine("Ano inválido. Insira um novo ano");
+                    ano = Convert.ToInt32(Console.ReadLine());
+                }
+            } while (!validador);
+            Ano = ano;
+        }
+
+        public string toString()
+        {
+            return Convert.ToString(Dia) + "/" + Convert.ToString(Mes) + "/" + Convert.ToString(Ano);
+        }
+
+        public void ProximoDia()
+        {
+            if (Dia == 30)
+            {
+                Dia = 1;
+                if (Mes == 12)
+                {
+                    Mes = 1;
+                    Ano = Ano + 1;
+                }
+                else
+                    Mes = Mes + 1;
+            }
+            else
+            {
+                Dia = Dia + 1;
+            }
+        }
+    }
+}
---
+++ Dev2B/Aula2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Aula2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //exercicio1();
+            //exercicio2();
+            exercicio3();
+            //exercicio4();
+        }
+
+        static void exercicio1()
+        {
+            //ItemVenda item = new ItemVenda(475, "Caixa de chocolate", -1, 7.80m);
+
+            ItemVenda item = new ItemVenda();
+
+            Console.Write("Digite o código do item: ");
+            item.NumeroItem = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a descrição do item: ");
+            item.Descricao = Console.ReadLine();
+
+            Console.Write("Digite a quatidade de item comprada: ");
+            item.QuantidadeComprada = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o preço unitário do item: ");
+            item.PrecoUnitario = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            item.EscreverProduto();
+ 
+        }
+
+        static void exercicio2()
+        {
+            //Empregado empregado = new Empregado("Yuri", "Fabio", 3000m);
+
+            Empregado empregado = new Empregado();
+
+            Console.Write("Digite o nome do empregado: ");
+            empregado.Nome = Console.ReadLine();
+
+            Console.Write("Digite o sobrenome do empregado: ");
+            empregado.SobreNome = Console.ReadLine();
+
+            Console.Write("Digite o salário mensal do empregado: ");
+            empregado.SalarioMensal = decimal.Parse(Console.ReadLine());
+
+            empregado.EscreverEmpregado();
+        }
+
+        static void exercicio3()
+        {
+            Data data = new Data();
+
+            Console.Write("Digite um dia: ");
+            data.Dia = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite um mês: ");
+            data.Mes = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite um ano: ");
+            data.Ano = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine($"Dia de hoje: {data.toString()}");
+            data.ProximoDia();
+            Console.WriteLine($"Próximo dia: {data.toString()}");
+        }
+
+        static void exercicio4()
+        {
+
+        }
+    }
+}
