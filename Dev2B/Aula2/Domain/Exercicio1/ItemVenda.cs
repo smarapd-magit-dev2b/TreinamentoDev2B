611c3093c342c423c18c4feb77a7d56c16a5ea55
SMARAPD - Filipe Cardoso <FACCARDOSO@SMARAPD.COM.BR>
2021-06-24 14:34:09 -0300

    Add aula2

---
+++ Dev2B/Aula2/Domain/Exercicio1/ItemVenda.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class ItemVenda
+    {
+        private int NumeroItem { get; set; }
+        private string Descricao { get; set; }
+        private int QtdComprada { get; set; }
+        private decimal PrecoUnit { get; set; }
+
+        public ItemVenda (int numeroitem, string descricao, int quantidade, decimal preco)
+        {
+            if (QtdComprada < 0)
+                QtdComprada = 0;
+            else
+                QtdComprada = quantidade;
+            if (PrecoUnit < 0)
+                PrecoUnit = 0;
+            else
+                PrecoUnit = preco;
+            NumeroItem = numeroitem;
+            Descricao = descricao;
+            QtdComprada = quantidade;
+            PrecoUnit = preco;
+        }
+        public decimal calcularValorTotal()
+        {
+            return PrecoUnit * QtdComprada;
+        }
+
+        public void EscreverInformacoes()
+        {
+            Console.WriteLine($"Número do item: {NumeroItem}");
+            Console.WriteLine($"Descrição: {Descricao}");
+            Console.WriteLine($"Quantidade: {QtdComprada} ");
+            Console.WriteLine($"Preço Unitário: {PrecoUnit} ");
+            Console.WriteLine($"Valor total: {calcularValorTotal()} ");
+        }
+    }
+}
---
+++ Dev2B/Aula2/Domain/Exercicio4/Professor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Exercicio4
+{
+    public class Professor
+    {
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string Nacionalidade { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string CNH { get; set; }
+        public DateTime DataAdmissao { get; set; }
+        public int RegistroProfessor { get; set; }
+        public decimal Salario { get; set; }
+        public Disciplina Disciplina { get; set; }
+
+        public Professor()
+        {
+            Disciplina = new Disciplina();
+        }
+    }
+}
