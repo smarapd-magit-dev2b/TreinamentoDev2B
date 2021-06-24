91fd37b5e99268c6d75dc9e40a09fdcc76c4aec0
Otaviooliv <otaviooliv2505@hotmail.com>
2021-06-24 13:53:19 -0300

    Aula 2

---
+++ Dev2B/Aula2_OOP/Exercicio_2_OOP/Exercicio_2_OOP/Program.cs
@@ -0,0 +1,51 @@
+﻿using Classes;
+using System;
+
+namespace Exercicio_2_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int escolha;
+
+            empregado Empregado1 = new empregado("João", "Paulo", 2000.00m);
+            empregado Empregado2 = new empregado("Pedro", "Augusto", 1500.00m);
+            empregado Empregado3 = new empregado("João", "Paulo", 3000.00m);
+
+            Console.WriteLine("1) Monstrar os empregados.\n2) Mostrar salario anual dos empregados.\n3) Fazer reajuste de 25% no salario dos empregados.\nResposta: ");
+            escolha = Convert.ToInt32(Console.ReadLine());
+
+            switch (escolha)
+            {
+                case 1:
+                    {
+                        Empregado1.dados();
+                        Empregado2.dados();
+                        Empregado3.dados();
+                        break;
+                    }
+                case 2:
+                    {
+                        Empregado1.Anaulidade();
+                        Empregado2.Anaulidade();
+                        Empregado3.Anaulidade();
+                        break;
+                    }
+                case 3:
+                    {
+                        Empregado1.Reajuste();
+                        Empregado2.Reajuste();
+                        Empregado3.Reajuste();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Desligando...");
+                        break;
+                    }
+
+            }
+        }
+    }
+}
---
+++ Dev2B/Aula2_OOP/Exercicio_4_OOP/Exercicio_4_OOP/Program.cs
@@ -0,0 +1,67 @@
+﻿using Escola;
+using System;
+
+namespace Exercicio_4_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno Aluno = new Aluno("Gabriel") {
+
+                Endereco = "Tijolos Amarelos",
+                Email = "gab@gmail.com",
+                Nacionalidade = "Brasileiro",
+                Cidade = "Ribeirã Preto",
+                Estado = "SP",
+                Telefone = "(16)12345-7890",        
+                RA = "2468791",
+            };
+
+
+            Console.WriteLine("Inserção de dados de professor!");
+            Console.WriteLine("Nome: ");
+            Aluno.Professor.Nome = Console.ReadLine();
+            Console.WriteLine("Endereço: ");
+            Aluno.Professor.Endereco = Console.ReadLine();
+            Console.WriteLine("Estado: ");
+            Aluno.Professor.Estado = Console.ReadLine();
+            Console.WriteLine("Nacionalidade : ");
+            Aluno.Professor.Nacionalidade = Console.ReadLine();
+            Console.WriteLine("Cidade: ");
+            Aluno.Professor.Cidade = Console.ReadLine();
+            Console.WriteLine("CNH: ");
+            Aluno.Professor.CNH = Console.ReadLine();
+            Console.WriteLine("Registro: ");
+            Aluno.Professor.Registro = Console.ReadLine();
+            Console.WriteLine("Salario: ");
+            Aluno.Professor.Salario = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine($"Curso que o {Aluno.Professor.Nome} fez: ");
+            Aluno.Professor.Disciplina.Curso = Console.ReadLine();
+            Console.WriteLine($"Nome do curso feito pelo Professor {Aluno.Professor.Nome}");
+            Aluno.Professor.Disciplina.Nome = Console.ReadLine();
+            Console.WriteLine($"CID do curso feito pelo Professor {Aluno.Professor.Nome}");
+            Aluno.Professor.Disciplina.CID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Data de inclusão ao curso de {Aluno.Professor.Disciplina.Curso}, que esta sendo feito pelo Professor {Aluno.Professor.Nome}");
+            Aluno.Professor.Disciplina.Nome = Console.ReadLine();
+
+
+
+            Console.WriteLine("");
+            Aluno.getDados();
+            Aluno.Professor.dadosProfessor();
+            
+            
+
+            Console.ReadLine();
+
+
+            
+                
+
+            
+            
+
+        }
+    }
+}
