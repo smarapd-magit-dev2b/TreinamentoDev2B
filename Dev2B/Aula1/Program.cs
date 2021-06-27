38620ed08a172a689812ed2170c87039986d3f69
jardeel <jardeelsousa@hotmail.com>
2021-06-27 18:15:48 -0300

    Adicionando Exercicio 01 e 02 da Aula 03

--- Dev2B/Aula1/Program.cs
+++ Dev2B/Aula1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Aula01;
 using Aula2;
+using Aula3;
 //using System;
 //using System.Collections.Generic;
 //using System.Linq;
@@ -12,11 +13,15 @@ namespace Application
     {
         static void Main(string[] args)
         {
-            ClassesObjetos classesObjetos = new ClassesObjetos();
             //EstruturasComando estruturasComando = new EstruturasComando();
-                
-            classesObjetos.Execute();
+            //ClassesObjetos classesObjetos = new ClassesObjetos(); 
+            //HerancasClassesAbstratasInterfaces herancasClassesAbstratasInterfaces = new HerancasClassesAbstratasInterfaces();
+            HerancasClassesAbstratasInterfaces herancasClassesAbstratasInterfaces = new HerancasClassesAbstratasInterfaces();
+
             //estruturasComando.Execute();
+            //classesObjetos.Execute();
+            herancasClassesAbstratasInterfaces.Execute();
+
         }
     }
 }
