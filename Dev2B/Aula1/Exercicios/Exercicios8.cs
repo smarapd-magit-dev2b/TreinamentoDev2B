28c78cf3e75363f955144231ec4ac9960b8d98bc
SMARAPD - Lucas Oliveira <LGOLIVEIRA@SMARAPD.COM.BR>
2021-06-28 10:40:28 -0300

    Aula 1 movida para o forma de classes

--- Dev2B/Aula1/Exercicios/Exercicios8.cs
+++ Dev2B/Aula1/Exercicios/Exercicios8.cs
@@ -14,6 +14,7 @@ namespace Aula1.Exercicios
         private void Exercicio8for()
         {
             Console.WriteLine("Exercício 8 For\n");
+
             for (int i = 3; i < 30; i++)
                 if ((i % 3) == 0 || (i % 4) == 0)
                     Console.WriteLine(i);
@@ -22,11 +23,14 @@ namespace Aula1.Exercicios
         private void Exercicio8while()
         {
             Console.WriteLine("Exercício 8 While\n");
-            int i = 3;
+
+            var i = 3;
+
             while (i < 30)
             {
                 if ((i % 3) == 0 || (i % 4) == 0)
                     Console.WriteLine(i);
+
                 i++;
             }
         }
@@ -34,11 +38,14 @@ namespace Aula1.Exercicios
         private void Exercicio8do()
         {
             Console.WriteLine("Exercício 8 Do While\n");
-            int i = 3;
+
+            var i = 3;
+
             do
             {
                 if ((i % 3) == 0 || (i % 4) == 0)
                     Console.WriteLine(i);
+
                 i++;
             } while (i < 30);
         }
