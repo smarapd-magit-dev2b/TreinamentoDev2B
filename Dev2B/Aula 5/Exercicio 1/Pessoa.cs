1b2b6d7de691dc6eaf345c8ca092e2608333c9c2
Abner <awmmanha@hotmail.com>
2021-06-29 16:21:13 -0300

    Exercicio 1 Aula 5

---
+++ Dev2B/Aula 5/Exercicio 1/Pessoa.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex1
+{
+    public class Pessoa
+    {
+       //Propriedades----------------------
+       public string Nome { get; set; }
+
+       public int Telefone { get; set; }
+
+       public DateTime DataNasc { get; set; }
+
+       public decimal Salario { get; set; }
+
+       public int QuantidadeFilho { get; set; }
+
+       public decimal Altura { get; set; }
+
+       public decimal Peso { get; set; }
+
+       public string NomePai { get; set; }
+
+       public string NomeMae { get; set; }
+
+        
+    
+    
+    
+    
+    
+    
+    }
+}
---
+++ Dev2B/Aula 5/Exercicio 1/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variavel----------------------
+            
+            int val;
+
+            var listaPessoa = new List<Pessoa>();
+            {
+            };
+
+            do
+            {
+                Console.WriteLine("\n * * * * * M E N U * * * * * ");
+
+                Console.WriteLine("\n [1] Cadastrar nova pessoa ");
+
+                Console.WriteLine("\n [2] Listar o nome de todas a as pessoas cadastradas");
+
+                Console.WriteLine("\n [9] Sair ");
+
+                Console.WriteLine("\n Digite a Opção");
+
+                val = Convert.ToInt32(Console.ReadLine());
+               
+                Console.Clear();
+
+                switch (val)
+                {
+
+
+                    case 1:
+
+                        Console.Write("Cadastro da pessoa");
+
+                        Console.Write("Nome: ");
+                        var nome = Console.ReadLine();
+
+                        Console.Write("Telefone: ");
+                        var telefone = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Data de Nascimento: ");
+                        var datanasc = Convert.ToDateTime(Console.ReadLine());
+
+                        Console.Write("Salario: ");
+                        var salario = Convert.ToDecimal(Console.ReadLine());
+
+                        Console.Write("Quantidade de filho: ");
+                        var quantidadefilho = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Altura: ");
+                        var altura = Convert.ToDecimal(Console.ReadLine());
+
+                        Console.Write("Peso: ");
+                        var peso = Convert.ToDecimal(Console.ReadLine());
+
+                        Console.Write("Nome do Pai: ");
+                        var nomepai = Console.ReadLine();
+
+                        Console.Write("Nome da mãe: ");
+                        var nomemae = Console.ReadLine();
+                        
+                        Console.Clear();
+
+                        listaPessoa.Add(new Pessoa
+                        {
+                            Nome = nome,
+                            Telefone = telefone,
+                            DataNasc = datanasc,
+                            Salario = salario,
+                            QuantidadeFilho = quantidadefilho,
+                            Altura = altura,
+                            Peso = peso,
+                            NomePai = nomepai,
+                            NomeMae = nomemae,
+                        }) ;
+
+                        break;
+
+                    case 2:
+
+                        foreach( var pessoa in listaPessoa)
+                        {
+                            
+                            Console.Write($"Nome: {pessoa.Nome}" );
+                             
+                        }
+
+                        break;
+
+
+                }
+
+
+            } while (val != 9);
+
+
+        }
+    }
+}
