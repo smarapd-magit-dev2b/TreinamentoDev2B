faf672a0f2c43d516fecf41b4a33da65414e8c5b
jardeel <jardeelsousa@hotmail.com>
2021-06-30 23:34:25 -0300

    Adicionando Aula01

---
+++ Dev2B/Application/Program.cs
@@ -0,0 +1,16 @@
+﻿using Aula01;
+using System;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World! Application");
+            EstruturasComando estruturasComando = new EstruturasComando();
+
+            estruturasComando.Exercicio();
+        }
+    }
+}
---
+++ Dev2B/Aula01/EstruturasComando.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Aula01
+{
+    public class EstruturasComando
+    {
+        public void Exercicio()
+        {
+            Console.WriteLine("Estrutura de Comando");
+        }
+
+        //Exercicio();
+    }
+}
