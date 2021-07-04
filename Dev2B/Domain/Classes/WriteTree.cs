8b22ef95a4c25371bad438169769ccb4f1dcb791
LuDColl <lucas.guimaraes.lc@gmail.com>
2021-07-03 21:34:23 -0300

    Write Treee otimizado

--- Dev2B/Domain/Classes/WriteTree.cs
+++ Dev2B/Domain/Classes/WriteTree.cs
@@ -1,40 +1,48 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Domain.Classes
 {
     public static class WriteTree
     {
-        public static void Propriedades(object objeto, params string[] titulos)
+        public static void Propriedades(object objeto,
+                                        params string[] titulos) => SubPropriedades(objeto, "   ", titulos.ToList(), 0);
+
+        private static void SubPropriedades(object objeto,
+                                            string tabulacao,
+                                            List<string> titulos,
+                                            int titulo)
         {
             var classe = objeto.GetType();
-            var titulosList = titulos.ToList();
 
-            if (!titulosList.Any())
-                Console.WriteLine($"\n{classe.Name}\n");
+            if (titulos.Count > titulo)
+                Console.WriteLine($"\n{tabulacao}{titulos[titulo]}\n");
             else
-            {
-                Console.WriteLine($"\n{titulosList.First()}\n");
-                _ = titulosList.Remove(titulosList.First());
-            }
+                Console.WriteLine($"\n{tabulacao}{classe.Name}\n");
 
             foreach (var propriedade in classe.GetProperties())
             {
-                if (propriedade.PropertyType.Namespace == "System")
-                    Console.WriteLine($"{propriedade.Name}: {propriedade.GetValue(objeto)}");
-                else if (propriedade.PropertyType.Namespace == "System.Collections.Generic")
+                switch (propriedade.PropertyType.Namespace)
                 {
-                    var lista = (IEnumerable)propriedade.GetValue(objeto);
-                    if (lista != null)
-                    {
-                        Console.WriteLine($"\n{propriedade.Name}\n");
+                    case "System":
+                        Console.WriteLine($"{tabulacao}{propriedade.Name}: {propriedade.GetValue(objeto)}");
+                        break;
+
+                    case "System.Collections.Generic":
+                        if (!(propriedade.GetValue(objeto) is IEnumerable lista))
+                            break;
+
+                        Console.WriteLine($"\n{tabulacao}{propriedade.Name}\n");
                         foreach (var item in lista)
-                            Propriedades(item);
-                    }
+                            SubPropriedades(item, $"{tabulacao}   ", titulos, titulo + 1);
+                        break;
+
+                    default:
+                        SubPropriedades(propriedade.GetValue(objeto), $"{tabulacao}   ", titulos, titulo + 1);
+                        break;
                 }
-                else
-                    Propriedades(propriedade.GetValue(objeto));
             }
             Console.WriteLine();
         }
