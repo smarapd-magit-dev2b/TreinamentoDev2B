bed090edf183ea73617bd0f7bb53254632772a64
LuDColl <lucas.guimaraes.lc@gmail.com>
2021-07-30 00:28:03 -0300

    Adiciona exercício 2 de JS

--- Dev2B/FrontEnd/Exercicios/03/exercicio.js
+++ Dev2B/FrontEnd/Exercicios/03/exercicio.js
@@ -3,3 +3,9 @@ console.log('Exercício 1')
 var cumprimentar = (nome) => `Olá ${nome}!`
 
 console.log(cumprimentar('Maurício'))
+
+console.log('\nExercício 2')
+
+var converterIdadeEmAnosParaDias = (idade) => idade * 365
+
+console.log(converterIdadeEmAnosParaDias(29))
