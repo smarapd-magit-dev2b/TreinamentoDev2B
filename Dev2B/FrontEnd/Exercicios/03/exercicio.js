daca61ca25c92d6202b3fef4768b85794b927fe5
LuDColl <lucas.guimaraes.lc@gmail.com>
2021-07-30 00:28:25 -0300

    Adiciona exercício 3 de JS

--- Dev2B/FrontEnd/Exercicios/03/exercicio.js
+++ Dev2B/FrontEnd/Exercicios/03/exercicio.js
@@ -9,3 +9,39 @@ console.log('\nExercício 2')
 var converterIdadeEmAnosParaDias = (idade) => idade * 365
 
 console.log(converterIdadeEmAnosParaDias(29))
+
+console.log('\nExercício 3')
+
+var nomeDoMes = (mes) => {
+  switch (mes) {
+    case 1:
+      return 'Janeiro'
+    case 2:
+      return 'Fevereiro'
+    case 3:
+      return 'Março'
+    case 4:
+      return 'Abril'
+    case 5:
+      return 'Maio'
+    case 6:
+      return 'Junho'
+    case 7:
+      return 'Julho'
+    case 8:
+      return 'Agosto'
+    case 9:
+      return 'Setembro'
+    case 10:
+      return 'Outubro'
+    case 11:
+      return 'Novembro'
+    case 12:
+      return 'Dezembro'
+    default:
+      return `${mes} é um mês inválido`
+  }
+}
+
+console.log(nomeDoMes(2))
+console.log(nomeDoMes(4))
