39190a321ea0d10e24f9eef198caa0c113df0867
LuDColl <lucas.guimaraes.lc@gmail.com>
2021-08-01 12:46:31 -0300

    Adiciona exercício 17 de JS

--- Dev2B/FrontEnd/Exercicios/03/exercicio.js
+++ Dev2B/FrontEnd/Exercicios/03/exercicio.js
@@ -223,6 +223,24 @@ somarNumeros = (array = []) => array.reduce((total, valor) => total + valor);
 console.log(somarNumeros([10, 10, 10]));
 console.log(somarNumeros([15, 15, 15, 15]));
 
+console.log('\nExercício 17');
+
+despesasTotais = (array = [{ preco }]) =>
+  array.reduce((total, valor) => total.preco + valor.preco);
+
+console.log(
+  despesasTotais([
+    { nome: 'Jornal online', categoria: 'Informação', preco: 89.99 },
+    { nome: 'Cinema', categoria: 'Entretenimento', preco: 150 },
+  ])
+);
+console.log(
+  despesasTotais([
+    { nome: 'Galaxy S20', categoria: 'Eletrônicos', preco: 3599.99 },
+    { nome: 'Macbook Pro', categoria: 'Eletrônicos', preco: 30999.9 },
+  ])
+);
+
 }
 
 console.log(estaEntre(51, 50, 100))
